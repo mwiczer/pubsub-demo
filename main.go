b67f58633187e6fc12cd18c0ba552347c22f0541
Michael Wiczer <wiczerm@gmail.com>
2024-05-26 11:57:54 -0400

    Add context awareness
    By putting `case <-ctx.Done():` in a branch of a select block, we can:
    1. Protect from deadlocks, for instance when writing to an unbuffered channel with no reader.
    2. Gracefully end our goroutines. For instance, finish processing the last message before exiting.
    
    If we want channel reads to be interruptible, then we can't use nice syntax things like `for msg := range myChannel`, because context is just a library, not something built in to the language. But I think it's pretty much always worth it, because it's too easy to get things wrong and cause deadlocks.

--- main.go
+++ main.go
@@ -2,30 +2,46 @@
 package main
 
 import (
+	"context"
 	"log"
 	"time"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
 	ch := make(chan string)
 
 	// Listen for messages
-	go func() {
+	go func(ctx context.Context) {
 		for {
-			msg := <-ch
-			log.Printf("Received message: %q", msg)
+			select {
+			case msg := <-ch:
+				log.Printf("Received message: %q", msg)
+			case <-ctx.Done():
+				log.Printf("Exiting listener: %v", ctx.Err())
+				return
+			}
 			time.Sleep(500 * time.Millisecond)
 		}
-	}()
+	}(ctx)
 
 	// Publish the messages
 	messages := []string{
 		"foo", "bar", "baz", "abc", "def",
 	}
+PublishLoop:
 	for i, msg := range messages {
 		log.Printf("Sending message %d...", i)
-		ch <- msg
+		select {
+		// Channel writing can be a branch of a select as well as channel reading.
+		case ch <- msg:
+		case <-ctx.Done():
+			log.Printf("Canceling channel write: %v", ctx.Err())
+			break PublishLoop
+		}
 	}
 	// Extra sleep to let the listener receive the last message.
 	time.Sleep(500 * time.Millisecond)
