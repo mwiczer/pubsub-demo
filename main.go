2af3c961bf94f62308b2f91228acd5a0376e6cb2
Michael Wiczer <wiczerm@gmail.com>
2024-05-26 12:07:14 -0400

    Hardcode a second subscriber
    - Before we extend to N subscribers, it's helpful to write it out long-hand.

--- main.go
+++ main.go
@@ -12,19 +12,59 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
-	ch := make(chan string)
+	pubCh := make(chan string)
+	subCh1 := make(chan string)
+	subCh2 := make(chan string)
+	subscribers := []chan string{subCh1, subCh2}
+
+	// Fanout the messages:
+	go func(ctx context.Context) {
+		i := 0
+		for {
+			var msg string
+			select {
+			case msg = <-pubCh:
+			case <-ctx.Done():
+				log.Printf("Exiting fanout goroutine: %v", ctx.Err())
+				return
+			}
+			i++
+			log.Printf("Forwarding message %d to subscribers...", i)
+			for _, subCh := range subscribers {
+				select {
+				case subCh <- msg:
+				case <-ctx.Done():
+					log.Printf("Exiting fanout goroutine: %v", ctx.Err())
+					return
+				}
+			}
+		}
+	}(ctx)
 
 	// Listen for messages
 	go func(ctx context.Context) {
 		for {
 			select {
-			case msg := <-ch:
-				log.Printf("Received message: %q", msg)
+			case msg := <-subCh1:
+				log.Printf("Received message on sub1: %q", msg)
 			case <-ctx.Done():
-				log.Printf("Exiting listener: %v", ctx.Err())
+				log.Printf("Exiting listener 1: %v", ctx.Err())
 				return
 			}
 			time.Sleep(500 * time.Millisecond)
+		}
+	}(ctx)
+
+	// Listen for messages
+	go func(ctx context.Context) {
+		for {
+			select {
+			case msg := <-subCh2:
+				log.Printf("Received message on sub2: %q", msg)
+			case <-ctx.Done():
+				log.Printf("Exiting listener 2: %v", ctx.Err())
+				return
+			}
 		}
 	}(ctx)
 
@@ -37,7 +77,7 @@ PublishLoop:
 		log.Printf("Sending message %d...", i)
 		select {
 		// Channel writing can be a branch of a select as well as channel reading.
-		case ch <- msg:
+		case pubCh <- msg:
 		case <-ctx.Done():
 			log.Printf("Canceling channel write: %v", ctx.Err())
 			break PublishLoop
