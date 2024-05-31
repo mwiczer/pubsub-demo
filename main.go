ef730d79fc53ae09999b61c651fd02b6e8292d5e
Michael Wiczer <wiczerm@gmail.com>
2024-05-30 20:40:40 -0400

    Extract the PubSub fanout runner
    - This represents the first approach for implementing a PubSub pattern with go channels.
    - It's a bit naive, and we'll discover some limitations shortly

--- main.go
+++ main.go
@@ -7,39 +7,58 @@ import (
 	"time"
 )
 
+type pubSubRunner struct {
+	// Arrows in the type annotation protect us from accidentally writing back into the Publisher
+	Publisher   <-chan string
+	Subscribers []chan<- string
+}
+
+func (ps pubSubRunner) Run(ctx context.Context) {
+	i := 0
+	for {
+		var msg string
+		// Receive from publisher
+		select {
+		case msg = <-ps.Publisher:
+		case <-ctx.Done():
+			log.Printf("Exiting fanout goroutine: %v", ctx.Err())
+			return
+		}
+		i++
+		// Fanout
+		log.Printf("Forwarding message %d to subscribers...", i)
+		for _, subCh := range ps.Subscribers {
+			select {
+			case subCh <- msg:
+			case <-ctx.Done():
+				log.Printf("Exiting fanout goroutine: %v", ctx.Err())
+				return
+			}
+		}
+	}
+}
+
+// RunPubSub creates a publisher channel which, when written to, fans the message out to all provided subscribers.
+//
+// It spawns a goroutine to manage the fanout. The publisher channel is unbuffered, therefore it is throttled by the slowest subscriber.
+func RunPubSub(ctx context.Context, subscribers ...chan<- string) chan<- string {
+	publisher := make(chan string)
+	ps := pubSubRunner{
+		Publisher:   publisher,
+		Subscribers: subscribers,
+	}
+	go ps.Run(ctx)
+	return publisher
+}
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
-	pubCh := make(chan string)
 	subCh1 := make(chan string)
 	subCh2 := make(chan string)
-	subscribers := []chan string{subCh1, subCh2}
-
-	// Fanout the messages:
-	go func(ctx context.Context) {
-		i := 0
-		for {
-			var msg string
-			select {
-			case msg = <-pubCh:
-			case <-ctx.Done():
-				log.Printf("Exiting fanout goroutine: %v", ctx.Err())
-				return
-			}
-			i++
-			log.Printf("Forwarding message %d to subscribers...", i)
-			for _, subCh := range subscribers {
-				select {
-				case subCh <- msg:
-				case <-ctx.Done():
-					log.Printf("Exiting fanout goroutine: %v", ctx.Err())
-					return
-				}
-			}
-		}
-	}(ctx)
+	pubCh := RunPubSub(ctx, subCh1, subCh2)
 
 	// Listen for messages
 	go func(ctx context.Context) {
